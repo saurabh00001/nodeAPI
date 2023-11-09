360ac57cdf6f662788e01cf833f3b1add9f01f4f
saurabh00001 <90037770+saurabh00001@users.noreply.github.com>
2023-11-09 08:57:14 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,11 +1,13 @@
 var mysql = require('mysql');
 var con = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'inspigenius'
+    host     : '162.240.96.135',
+    user     : 'remotedbinspizon_db',
+    password : 'V2tIt{qqyBa)',
+    database : 'remotedbinspizon_db'
 });
-
+// Host IP: 162.240.96.135
+// DB & USER NAME: remotedbinspizon_db
+// PASS: V2tIt{qqyBa)
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
